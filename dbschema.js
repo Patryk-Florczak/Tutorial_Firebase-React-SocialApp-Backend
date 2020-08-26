86bd9394542374b0df43db0fd88a36e3f15536a8
Patryk Florczak <patrykflorczak13@gmail.com>
2020-08-26 22:15:55 +0200

    requests refactoring

---
+++ dbschema.js
@@ -0,0 +1,13 @@
+// Schema for posts
+
+// let db = [
+//   screams: [
+//     {
+//       userHandle: 'user',
+//       body: 'this is scream body',
+//       createdAt: '2020-08-26T19:39:00.535Z',
+//       likeCount: 5,
+//       commentCount: 2
+//     },
+//   ]
+// ];
--- functions/index.js
+++ functions/index.js
@@ -3,48 +3,50 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
+const express = require('express');
+const app = express();
 
 // IMPORTANT!! USE firebase emulators:start to work
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
-});
-
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
-    .then(data => {
-      let screams = [];
-      data.forEach(doc => {
-        screams.push(doc.data());
+app.get('/screams', (req, res) => {
+  admin
+  .firestore()
+  .collection('screams')
+  .orderBy('createdAt', 'desc')
+  .get()
+  .then((data) => {
+    let screams = [];
+    data.forEach((doc) => {
+      screams.push({
+        screamId: doc.id,
+        body: doc.data().body,
+        userHandle: doc.data().userHandle,
+        createdAt: doc.data().createdAt
       });
-      return res.json(screams);
-    })
-    .catch(err => console.log(err))
+    });
+    return res.json(screams);
+  })
+  .catch((err) => console.log(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if(req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed' })
-  }
+app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
-  };
+    createdAt: new Date().toISOString()
+  }
 
-  admin.firestore()
+  admin
+    .firestore()
     .collection('screams')
     .add(newScream)
     .then((doc) => {
-      res.json({message: `document ${doc.id} created successfully`});
+      res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch((err) => {
-      res.status(500).json({ error: 'something went wrong'});
+      res.status(500).json({ error: 'something went wrong' });
       console.error(err);
     });
 });
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
