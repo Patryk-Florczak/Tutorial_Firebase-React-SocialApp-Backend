a5ffa42f9822e1860eaa74a05b2202fae239848f
Patryk Florczak <patrykflorczak13@gmail.com>
2020-08-28 23:13:05 +0200

    [refactor] split routes and helpers into separate files

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyAXHS2LgDykFPnxmDV7ffECPdevi1bFQ18',
+  authDomain: 'socialapp-51b1b.firebaseapp.com',
+  databaseURL: 'https://socialapp-51b1b.firebaseio.com',
+  projectId: 'socialapp-51b1b',
+  storageBucket: 'socialapp-51b1b.appspot.com',
+  messagingSenderId: '365202435345',
+  appId: '1:365202435345:web:434c0765dd290b207470b0',
+  measurementId: 'G-RN97SKMZPB',
+};
