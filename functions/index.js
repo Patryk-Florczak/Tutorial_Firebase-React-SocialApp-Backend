10a216d3b8945f67cdbb324eb6766dd56b881327
Patryk <projecttristis@gmail.com>
2020-09-01 23:21:24 +0200

    [add] get and add user credentials routes

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,13 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require('./handlers/users');
 
 // SCREAM ROUTES
 app.get('/screams', getAllScreams);
@@ -17,5 +23,7 @@ app.post('/scream', FBAuth, postOneScream);
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
